b4b39c39200f10a3164349bada7fa0d591e57380
James Stuckey <jstuckey@rutgers.edu>
2018-06-05 16:47:36 -0400

    updated server js to include mongod check

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-const { PORT } = require('./config');
+const { PORT, MONGODB_URI } = require('./config');
 
 const notesRouter = require('./routes/notes');
 
@@ -12,7 +13,7 @@ const app = express();
 
 // Log all requests. Skip logging during
 app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
-  skip: () => process.env.NODE_ENV === 'test'
+    skip: () => process.env.NODE_ENV === 'test'
 }));
 
 // Create a static webserver
@@ -26,29 +27,40 @@ app.use('/api/notes', notesRouter);
 
 // Custom 404 Not Found route handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // Custom Error Handler
 app.use((err, req, res, next) => {
-  if (err.status) {
-    const errBody = Object.assign({}, err, { message: err.message });
-    res.status(err.status).json(errBody);
-  } else {
-    console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
+    if (err.status) {
+        const errBody = Object.assign({}, err, { message: err.message });
+        res.status(err.status).json(errBody);
+    } else {
+        console.error(err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 });
 
+mongoose.connect(MONGODB_URI)
+    .then(instance => {
+        const conn = instance.connections[0];
+        console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+    })
+    .catch(err => {
+        console.error(`ERROR: ${err.message}`);
+        console.error('\n === Did you remember to start `mongod`? === \n');
+        console.error(err);
+    });
+    
 // Listen for incoming connections
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, function () {
-    console.info(`Server listening on ${this.address().port}`);
-  }).on('error', err => {
-    console.error(err);
-  });
+    app.listen(PORT, function () {
+        console.info(`Server listening on ${this.address().port}`);
+    }).on('error', err => {
+        console.error(err);
+    });
 }
 
 module.exports = app; // Export for testing
